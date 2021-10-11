15c9b9c10a04ed0238d4d4d62a9f555e1010f8a4
muhamadisro27 <mohammadisro2710@gmail.com>
2021-10-11 23:39:54 +0800

    menambahkan data dummy buku

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Beranda from "./components/Beranda";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ManajemenBuku from "./components/ManajemenBuku";
+import { useState } from "react";
 
 function App() {
+  const [books, setBooks] = useState([
+    {
+      _id: 1,
+      judul: "Laskar Pelangi",
+      pengarang: "Andrea Hirata",
+      harga: 80000,
+      stok: 7,
+    },
+    {
+      _id: 2,
+      judul: "Bumi",
+      pengarang: "Tere Liye",
+      harga: 85000,
+      stok: 5,
+    },
+  ]);
   return (
     <div>
       <BrowserRouter>
