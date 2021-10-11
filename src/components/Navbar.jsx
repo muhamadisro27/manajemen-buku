0b2160bdc99b69d2cb5c72ef6c9ebdc12663c2e2
muhamadisro27 <mohammadisro2710@gmail.com>
2021-10-11 23:02:43 +0800

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import logo from "../assets/camp.png";
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand bg-dark navbar-dark">
         <div className="container">
           <ul className="nav">
-            <img src="" alt="" />
+            <img src={logo} alt="" style={{ height: "40px" }} />
             <li>
               <a href="/" className="nav-link">
                 Beranda
@@ -23,3 +23,5 @@ const Navbar = () => {
     </div>
   );
 };
+
+export default Navbar;
