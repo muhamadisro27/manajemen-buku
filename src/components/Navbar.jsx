a8b5da2148e67a80026dc24151d8a25211921836
muhamadisro27 <mohammadisro2710@gmail.com>
2021-10-11 22:55:17 +0800

    menambahkan component navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand bg-dark navbar-dark">
+        <div className="container">
+          <ul className="nav">
+            <img src="" alt="" />
+            <li>
+              <a href="/" className="nav-link">
+                Beranda
+              </a>
+            </li>
+            <li>
+              <a href="/manajemen-buku" className="nav-link">
+                Manajemen Buku
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
