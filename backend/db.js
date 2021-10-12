a8eedda610b85cfa25b18e3a6b9ac8ccd529ba59
muhamadisro27 <mohammadisro2710@gmail.com>
2021-10-12 09:43:47 +0800

    menambahkan file model.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/db_buku", {
-  userNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true,
-});
+mongoose.connect("mongodb://localhost:27017/db_buku");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Koneksi db gagal.."));
